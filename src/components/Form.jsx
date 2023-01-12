c3930885d9776d432c4c529806afb307ac62fe79
Lívio Lopes <antonio.lvio760@gmail.com>
2023-01-12 09:41:29 -0300

    refatorando 2.0 requisito 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,5 @@
 import { Component } from 'react';
-import FormName from './FormName';
-import FormDescription from './FormDescription';
-import FormAttr1 from './FormAttr1';
-import FormAttr2 from './FormAttr2';
-import FormAttr3 from './FormAttr3';
-import FormImage from './FormImage';
-import FormRarity from './FormRarity';
-import FormTrunfo from './FormTrunfo';
+// import PropTypes from 'prop-types';
 import './Form.css';
 
 export default class Form extends Component {
@@ -15,16 +8,90 @@ export default class Form extends Component {
       <div>
         <h2>Adicione Nova Carta</h2>
         <form action="">
-          <FormName />
-          <FormDescription />
-          <FormAttr1 />
-          <FormAttr2 />
-          <FormAttr3 />
-          <FormImage />
-          <FormRarity />
-          <FormTrunfo />
+          <label htmlFor="name-input">
+            <span>Nome</span>
+            <input
+              type="text"
+              name="name-input"
+              data-testid="name-input"
+            />
+          </label>
+          <label htmlFor="description-input">
+            <span>Descrição</span>
+            <textarea
+              name="description"
+              id=""
+              cols="30"
+              rows="10"
+              data-testid="description-input"
+            />
+          </label>
+          <label htmlFor="attr1-input">
+            <span>Attr01</span>
+            <input
+              type="number"
+              name="attr1-input"
+              data-testid="attr1-input"
+            />
+          </label>
+          <label htmlFor="attr2-input">
+            <span>Attr02</span>
+            <input
+              type="number"
+              name="attr2-input"
+              data-testid="attr2-input"
+            />
+          </label>
+          <label htmlFor="attr3-input">
+            <span>Attr03</span>
+            <input
+              type="number"
+              name="attr3-input"
+              data-testid="attr3-input"
+            />
+          </label>
+          <label htmlFor="image-input">
+            <span>Imagem</span>
+            <input
+              type="text"
+              name="image-input"
+              data-testid="image-input"
+            />
+          </label>
+          <label htmlFor="rare-input">
+            <span>Raridade</span>
+            <select
+              name="rare-input"
+              id=""
+              data-testid="rare-input"
+            >
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+          <label htmlFor="trunfo-input">
+            <span>Super Trunfo</span>
+            <input
+              type="checkbox"
+              name="trunfo-input"
+              data-testid="trunfo-input"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="save-button"
+          >
+            Salvar
+
+          </button>
         </form>
       </div>
     );
   }
 }
+
+// Form.propTypes = {
+//   cardName: PropTypes.string.isRequired,
+//   onInputChange: PropTypes.func.isRequired,
+// };
